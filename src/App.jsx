fc7129b8a7c38c6c36100e0ae2c3cfb8518b87b3
frankleen99 <waydenonso@gmail.com>
2024-09-12 16:43:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p className='bg-red-400'>
+      hello from password auth
+      </p>
+    </div>
+  )
+}
+
+export default App
